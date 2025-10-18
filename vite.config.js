c29065dfd9fa80446e39f12314bc69ed0122094b
Alex Wang <selltrends@gmail.com>
2025-10-18 15:39:10 +0800

    add asset static file copy

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 const { resolve } = require('path');
 
 export default defineConfig({
     plugins: [
         tailwindcss(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: resolve(__dirname, 'static/images/**/*'),
+                    dest: 'images',
+                },
+            ],
+        }),
     ],
     base: '/static/',
     server: {
