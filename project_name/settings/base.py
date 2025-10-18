aad75e32ed761aadd999e9fffb8dd371e38b0155
Alex Wang <selltrends@gmail.com>
2025-10-18 15:22:28 +0800

    fix template errors

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -201,7 +201,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "assets"),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "stastaticfilestic")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 
 MEDIA_ROOT = os.environ.get("MEDIA_ROOT", os.path.join(BASE_DIR, "media"))
